1ae5df792a1d9f278e08d92d5dfa1c7cd3a13ea6
Nick Hosman <nickhosman@gmail.com>
2023-10-08 00:31:33 -0700

    styling for log in modal

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -6,55 +6,69 @@ import "./LoginForm.css";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function LoginFormModal() {
-  const dispatch = useDispatch();
-  const history = useHistory()
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-  const { closeModal } = useModal();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([]);
+    const { closeModal } = useModal();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = await dispatch(login(email, password));
-    if (data) {
-      setErrors(data);
-    } else {
-        closeModal()
-        history.push("/posts")
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = await dispatch(login(email, password));
+        if (data) {
+            setErrors(data);
+        } else {
+            closeModal();
+            history.push("/posts");
+        }
+    };
 
-  return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
-    </>
-  );
+    const handleDemo = async (e) => {
+        e.preventDefault();
+        await dispatch(login("demo@aa.io", "password"));
+        closeModal();
+        history.push("/posts");
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit} id="log-in-form">
+                <h1>Log In</h1>
+                <ul>
+                    {errors.map((error, idx) => (
+                        <li key={idx}>{error}</li>
+                    ))}
+                </ul>
+                <p className="log-in-label-text">Email</p>
+                <label className="log-in-label">
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        className="log-in-input"
+                    />
+                </label>
+                <p className="log-in-label-text">Password</p>
+                <label className="log-in-label">
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        className="log-in-input"
+                    />
+                </label>
+                <button type="submit" id="log-in-form-btn">
+                    Log In
+                </button>
+                <button type="submit" id="log-in-demo" onClick={handleDemo}>
+                    Demo User
+                </button>
+            </form>
+        </>
+    );
 }
 
 export default LoginFormModal;
