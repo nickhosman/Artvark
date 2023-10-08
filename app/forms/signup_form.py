2394a77d0f6f34d81e36e94b5d1bd4d27128c4d8
Nick Hosman <nickhosman@gmail.com>
2023-10-07 20:13:52 -0700

    styling and refactoring auth route for sign up

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-# from flask_wtf.file import FileField, FileAllowed, FileRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError, URL
 from app.models import User
@@ -29,4 +29,4 @@ class SignUpForm(FlaskForm):
     password = StringField('password', validators=[DataRequired()])
     first_name = StringField('first_name', validators=[DataRequired()])
     last_name = StringField('last_name', validators=[DataRequired()])
-    # profile_img = FileField('Profile Image', validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+    profile_img = FileField("image", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
